6eeb29a7dc91bd0c3947c7aed43d39be31b1a3fc
Mudit Choudhary <cmudit60@gmail.com>
2022-09-23 08:52:12 +0530

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function component() {
 	// Lodash, currently included via a script, is required for this line to work
 	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
+	element.style.color = 'blue';
 
 	return element;
 }
